68895313d3abf6b6992c5c3c1f0732b21e0504e7
MuhammadAyan-Programmer <chayanprofesional@gmail.com>
2026-09-18 17:33:43 +0500

    chore: update admin credentials and remove API entrypoint
    Update admin password hash for security and remove deprecated Vercel serverless function entrypoint.

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-// Vercel Serverless Function entrypoint
-// @ts-ignore
-import server from '../dist/server.cjs';
-
-const app = (server as any).default?.default || (server as any).default || server;
-
-export default app;
